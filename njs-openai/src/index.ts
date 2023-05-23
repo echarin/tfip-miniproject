2f05c595efe6ec0d4df89fea3f07ca99df9e0fdf
echarin <wanmingkang@gmail.com>
2023-05-23 19:34:04 +0800

    md additions, requirements.md

--- njs-openai/src/index.ts
+++ njs-openai/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Testing")
+import { Configuration, OpenAIApi } from 'openai';
+import { config } from 'dotenv';
+
+config(); // load environment variables
+
+async function main() {
+    const configuration = new Configuration({
+        organization: process.env.ORGANIZATION,
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "Hello world",
+        });
+
+        console.log(completion.data.choices[0].text);
+    } catch (error) {
+        console.error("An error occured: ", error);
+    }
+}
+
+// main();
