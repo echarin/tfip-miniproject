edba52e630f89472f63b28d6c10a05f2ce464b8b
echarin <wanmingkang@gmail.com>
2023-06-22 22:30:09 +0800

    added spring security test to pom.xml, new budgetcontrollertest, accessdeniedexception in globalexceptionhandler

--- sb-backend/src/main/java/ibf2022/tfipminiproject/exceptions/GlobalExceptionHandler.java
+++ sb-backend/src/main/java/ibf2022/tfipminiproject/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package ibf2022.tfipminiproject.exceptions;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 
 @RestControllerAdvice
@@ -23,5 +25,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(e.getMessage()));
     }
 }
