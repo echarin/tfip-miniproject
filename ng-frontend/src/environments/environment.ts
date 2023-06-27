a428ef392199cf1d59513558406a9906c27815a8
echarin <wanmingkang@gmail.com>
2023-06-28 01:35:34 +0800

    shuffled yaml files, new ng dockerfile

--- ng-frontend/src/environments/environment.ts
+++ ng-frontend/src/environments/environment.ts
@@ -2,6 +2,7 @@
 export const environment = {
     production: true,
     sbServerUrl: 'http://localhost:8080', // make this dynamic later
+    // For example: <service-name>.<namespace>.svc.cluster.local
     api: '/api/v1',
     auth: '/api/v1/auth'
 };
