9c9275494bd60ded63d02033b3c19ad71a9d73e0
echarin <wanmingkang@gmail.com>
2023-06-28 10:07:40 +0800

    test new ingress (after project deadline)

--- ng-frontend/src/environments/environment.ts
+++ ng-frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 // environment.ts
 export const environment = {
     production: true,
-    sbServerUrl: 'https://tart-throat-production.up.railway.app', // Railway
-    // Would be '' for Ingress
+    // sbServerUrl: 'https://tart-throat-production.up.railway.app', // Railway
+    sbServerUrl: 'http://sb-backend-service.default.svc.cluster.local', // Kubernetes
     api: '/api/v1',
     auth: '/api/v1/auth'
 };
