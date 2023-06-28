773d08938a3dce1a6e0ee47117f6adc9a4918702
echarin <wanmingkang@gmail.com>
2023-06-28 10:54:26 +0800

    test separate ingress (after project deadline)

--- ng-frontend/src/environments/environment.development.ts
+++ ng-frontend/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 // environment.development.ts
 export const environment = {
     production: false,
-    sbServerUrl: 'http://10.84.0.32:8080',
+    sbServerUrl: '', // Kubernetes, using Ingress
     api: '/api/v1',
     auth: '/api/v1/auth'
 };
--- ng-frontend/src/environments/environment.ts
+++ ng-frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
     production: true,
     // sbServerUrl: 'https://tart-throat-production.up.railway.app', // Railway
-    sbServerUrl: 'http://10.84.0.32:8080', // Kubernetes
+    sbServerUrl: '', // Kubernetes, using Ingress
     api: '/api/v1',
     auth: '/api/v1/auth'
 };
