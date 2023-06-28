e8dc51d2779122b56546fdf40c2b421b19ff60cd
echarin <wanmingkang@gmail.com>
2023-06-28 10:40:21 +0800

    change env (after project deadline)

--- ng-frontend/src/environments/environment.development.ts
+++ ng-frontend/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 // environment.development.ts
 export const environment = {
     production: false,
-    sbServerUrl: 'http://sb-backend-service.default.svc.cluster.local',
+    sbServerUrl: 'http://10.84.0.32:8080',
     api: '/api/v1',
     auth: '/api/v1/auth'
 };
--- ng-frontend/src/environments/environment.ts
+++ ng-frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
     production: true,
     // sbServerUrl: 'https://tart-throat-production.up.railway.app', // Railway
-    sbServerUrl: 'http://sb-backend-service.default.svc.cluster.local', // Kubernetes
+    sbServerUrl: 'http://10.84.0.32:8080', // Kubernetes
     api: '/api/v1',
     auth: '/api/v1/auth'
 };
