386c46b49f45f6d1312731689581787012d40786
echarin <wanmingkang@gmail.com>
2023-06-28 08:32:33 +0800

    test railway (outside project deadline)

--- ng-frontend/src/environments/environment.development.ts
+++ ng-frontend/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 // environment.development.ts
 export const environment = {
     production: false,
-    sbServerUrl: 'http://localhost:8080',
+    sbServerUrl: '',
     api: '/api/v1',
     auth: '/api/v1/auth'
 };
